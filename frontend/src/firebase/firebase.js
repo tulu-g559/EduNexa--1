af5a612ee946e5e4d1fb80036e348b90eb412642
archakkhandayit <archakkhandayit@gmail.com>
2025-03-21 12:56:59 +0530

    firebase and env

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -3,13 +3,13 @@ import {getAuth} from "firebase/auth";
 import {getFirestore} from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDhO14yl5dRdHLcwRNxXHWDWdLqCZ_Ubqw",
-  authDomain: "edunexa-test.firebaseapp.com",
-  projectId: "edunexa-test",
-  storageBucket: "edunexa-test.firebasestorage.app",
-  messagingSenderId: "466675539193",
-  appId: "1:466675539193:web:af4db8877ff5492d535628",
-  measurementId: "G-K4MKNN3VXT",
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+  measurementId: "",
 };
 
 
