21bf06abe4e0e718d20b1d9768ef51b77d87380b
archakkhandayit <archakkhandayit@gmail.com>
2025-03-27 15:25:04 +0530

    final code

--- frontend/src/firebase/firebase.example.js
+++ frontend/src/firebase/firebase.example.js
@@ -7,7 +7,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  //paste the config 
+    //paste firebaseconfig
 };
 
 // Initialize Firebase
