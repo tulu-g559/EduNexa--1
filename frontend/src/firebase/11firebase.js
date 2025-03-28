73fc1e628daf9b0ec17d222491c1f47153278d5d
tulu-g559 <garnab559@gmail.com>
2025-03-28 16:40:06 +0530

    Deploy 2

--- frontend/src/firebase/11firebase.js
+++ frontend/src/firebase/11firebase.js
@@ -6,10 +6,14 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-//Instruction: Create a file named firebase.js in path "/frontend/src/firebase/firebase.js"
-//This is just an example
 const firebaseConfig = {
-    //paste firebaseconfig
+    apiKey: "AIzaSyANtUjEQZHwwnp-tFijQK4S7cnwaiXHCGY",
+    authDomain: "edunexa-27a49.firebaseapp.com",
+    projectId: "edunexa-27a49",
+    storageBucket: "edunexa-27a49.firebasestorage.app",
+    messagingSenderId: "272901258354",
+    appId: "1:272901258354:web:5be2bf99b3558e737796eb",
+    measurementId: "G-Y89H1W3S3S"
 };
 
 // Initialize Firebase
