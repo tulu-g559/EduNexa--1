fab7e5e47cb5f80e9f92e6a6fe393b5fcd8adf66
tulu-g559 <garnab559@gmail.com>
2025-03-28 16:50:14 +0530

    IDK2

--- frontend/src/firebase/firebase.js
+++ frontend/src/firebase/firebase.js
@@ -6,20 +6,15 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-//Instruction: Create a file named firebase.js in path "/frontend/src/firebase/firebase.js"
-//This is just an example
 const firebaseConfig = {
-    apiKey: "AIzaSyANtUjEQZHwwnp-tFijQK4S7cnwaiXHCGY",
-    authDomain: "edunexa-27a49.firebaseapp.com",
-    projectId: "edunexa-27a49",
-    storageBucket: "edunexa-27a49.firebasestorage.app",
-    messagingSenderId: "272901258354",
-    appId: "1:272901258354:web:5be2bf99b3558e737796eb",
-    measurementId: "G-Y89H1W3S3S"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
-
-export default firebaseConfig;
-
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
